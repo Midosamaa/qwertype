1cc295be126fcbde150c318c7083c16169eeb501
Midosamaa <148008360+Midosamaa@users.noreply.github.com>
2024-08-02 04:53:45 +0200

    Add files via upload
    working prototype

--- qwertype/main.js
+++ qwertype/main.js
@@ -1,2 +1,3 @@
 
 launch_game()
+
--- qwertype/qwertype.js
+++ qwertype/qwertype.js
@@ -1,75 +1,40 @@
-console.log("Hello World");
 
 
-
-
-function choose_category(){
-    let category_choice=prompt("Do you want the words quizz, or the sentences quizz?")
-    while((category_choice!== "words") && (category_choice!=="sentences")){
-        category_choice=prompt("Do you want the words quizz, or the sentences quizz?")
-    }   
-    return category_choice
+function disp_score(score, total_questions) {
+    document.querySelector(".score_zone span").innerHTML=`<span>${score} / ${total_questions}</span>`;
 }
 
-function comment_score(score, total_questions){
-    let comment="Your score is "+score+"/"+total_questions
-    return comment
+function display_suggestion(word) {
+    document.querySelector(".suggestion_zone").textContent = word;
 }
 
-function game_loop(category_choice){
-    let score=0
-    if(category_choice==="words"){
-        for (let i=0; i<3; i++){
-    
-            let user_word = prompt("Enter the word : "+app_words[i])
-            if(user_word===app_words[i]){
-                console.log("Good job!")
-                score++
-            }
-            else{
-                console.log("You're just bad...")
-            } 
+function launch_game() {
+    let score = 0;
+    let i = 0;
+
+    const submit_button = document.getElementById("submit_btn");
+    const typing_tag = document.getElementById("writing_input");
+
+    function check_and_update() {
+        if (typing_tag.value === document.querySelector(".suggestion_zone").textContent) {
+            score++;
         }
+        disp_score(score, i + 1);
     }
-    else{
-        for (let i=0; i<3; i++){
-    
-            let user_word = prompt("Enter the sentence : "+app_sentences[i])
-            if(user_word===app_sentences[i]){
-                console.log("Good job!")
-                score++
-            }
-            else{
-                console.log("You're just bad...")
-            } 
+
+    submit_button.addEventListener("click", () => {
+        check_and_update();
+        i++;
+        if (app_words[i] !== undefined) {
+            display_suggestion(app_words[i]);
+        } else {
+            display_suggestion("Game Over");
+            submit_button.disabled = true;
         }
+        typing_tag.value = ""; // Clear input field after each submission
+    });
+
+    if (app_words.length > 0) {
+        display_suggestion(app_words[i]);
     }
-    return score
 }
-
-
-function launch_game(){
-
-    let category_choice=choose_category()
-    let score=game_loop(category_choice);
-    
-    switch (score){
-        case 1:
-            console.log("You're meh you should train some more")
-            break
-        case 2:
-            console.log("You're good but you can do better")
-            break
-        case 3:
-            console.log("Perfect")
-            break
-        default:
-            console.log("You're trash find something to do with your life")
-    }
-    
-    console.log(comment_score(score, 3))    
-}
-
-
-
-
